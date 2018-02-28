42a561f68b27b61868d29a3c718952b6ec0fec6c
Dominik Picheta <dominikpicheta@gmail.com>
2018-02-28 16:37:36 +0000

    Ask user whether to include a particular commit and support GH tokens.

--- relnotesgen.nim
+++ relnotesgen.nim
@@ -13,7 +13,7 @@ proc getCommitList(ver: string): seq[tuple[hash, desc: string]] =
   let (output, exitCode) = execCmdEx("git log --oneline --reverse " & ver &
       "..HEAD")
   for line in splitLines(output):
-    result.add((line[0 .. 6], line[7 .. ^1]))
+    result.add((line.substr(0, 6), line.substr(7)))
 
 proc parseCommit(commit: string, issues: var seq[Issue], next: int = 0): bool =
   var startAgainOn = 0
@@ -42,25 +42,43 @@ proc parseCommit(commit: string, issues: var seq[Issue], next: int = 0): bool =
      "conversion", "resolve", "fixup", "rfc", "close":
     result = true
     issue.status = Fixed
-  of "request", "apply", "issue", "ref", "shadowed", "stacktrace", "for",
-     "merged", "", "around", "by", "of", "to", "refs", "in", "gc-safe", "on",
-     "pr", "modified", "dictreader", "warning", "compileoption",
-     "close/unregister", "working", "format":
-    result = false # TODO: I think the keyword list above is pretty conclusive.
   else:
-    echo commit
-    assert false, word
+    echo "```\"", commit, "\": unsure whether to include this, should I? [y/n]"
+    let answer = stdin.readLine()
+    echo "```"
+    case answer.strip()
+    of "y", "yes":
+      result = true
+      issue.status = Fixed
+    of "n", "no":
+      result = false
+    else: doAssert false
+
   issues.add issue
   result = result or parseCommit(commit, issues, startAgainOn)
 
-proc getIssueTitle(repo: string, issue: int): string =
+proc getIssueTitle(repo: string, issue: int, token: string): string =
   let url = "https://api.github.com/repos/$1/issues/$2" % [repo, $issue]
-  let content = getContent(url)
+  let client = newHttpClient()
+  if token.len > 0:
+    client.headers = newHttpHeaders({
+      "Authorization": "token $1" % token
+    })
+  let content = client.getContent(url)
   let parsed = parseJson(content)
   return parsed["title"].str
 
 when isMainModule:
   let repo = "nim-lang/Nim"
+
+  let token = if paramCount() >= 1: paramStr(1) else: ""
+
+  if token.len == 0:
+    echo("WARNING: You have not specified a GitHub token as a command line " &
+         "argument. You will run into the rate limit quickly without it.")
+    sleep(2000)
+  else:
+    echo("Thank you for supplying your GitHub token")
 
   # Are we in a git dir?
   if not existsDir(getCurrentDir() / ".git"):
@@ -69,16 +87,16 @@ when isMainModule:
   # Issue number to start requests to GH after.
   # Until this issue number is found, the issues will not be echoed.
   # Use `-1` to start immediately.
-  let requestAfter = 5532
+  let requestAfter = -1
   # Get a list of commits.
   var started = requestAfter == -1
-  let commits = getCommitList("v0.16.0")
+  let commits = getCommitList("v0.17.2")
   for c in commits:
     var issues: seq[Issue] = @[]
     if parseCommit(c.desc, issues):
       for i in issues:
         if started:
-          let title = getIssueTitle(repo, i.number)
+          let title = getIssueTitle(repo, i.number, token)
           let url = "https://github.com/$1/issues/$2" % [repo, $i.number]
           let str = "- Fixed \"$1\"\n  ([#$2]($3))" % [title, $i.number, url]
           echo(str)
